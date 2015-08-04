45dbb924160a672e11d67fa2ed05d53bb4cd2e53
Michael Reeves <mreeves68@yahoo.com>
2015-08-04 13:06:00 -0600

    Implement Products#index and #show html

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
 
   def index
-    
+
   end
 
+  def products_index
+  end
+
+  def products_show
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: "static_pages#index"
+  resources :products, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
