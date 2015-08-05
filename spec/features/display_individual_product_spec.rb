b4d5985b3c1708d09b0ef08469d1f0d72603ab13
Michael Dorrance <dorrancemd@gmail.com>
2015-08-05 13:35:53 -0600

    Render product image and Feature Products on Product#show

--- spec/features/display_individual_product_spec.rb
+++ spec/features/display_individual_product_spec.rb
@@ -2,18 +2,50 @@ require "rails_helper"
 
 feature "a visitor" do
   context "visits /products/:id" do
-    scenario "and sees product details" do
-      product = Product.create(name: "Plant 1",
-                               description: "This is the description for plant 1",
-                               price: 19.99)
+    let(:product) do
+      Product.create(
+        name: "Plant 1",
+        description: "This is the description for plant 1",
+        price: 19.99,
+        image_url: "plants/plant-2.jpg")
+    end
 
+    scenario "and sees product details" do
       visit product_path(product)
 
       expect(page).to have_content("Plant 1")
       expect(page).to have_content("This is the description for plant 1")
       expect(page).to have_content("19.99")
+      expect(page).to have_xpath("//img[@src=\"/assets/plants/plant-2.jpg\"]")
       within(".caption-full") do
         expect(page).to have_content("Add to Cart")
+      end
+    end
+
+    scenario "and sees featured products" do
+      Product.create(
+        name: "Plant 2",
+        description: "This is the description for plant 2",
+        price: 29.99,
+        image_url: "plants/plant-4.jpg")
+      Product.create(
+        name: "Plant 3",
+        description: "This is the description for plant 3",
+        price: 39.99,
+        image_url: "plants/plant-3.jpg")
+
+      visit product_path(product)
+
+      within("#product-carousel") do
+        expect(page).to have_content("Plant 1")
+        expect(page).to have_content("19.99")
+        expect(page).to have_xpath("//img[@src=\"/assets/plants/plant-2.jpg\"]")
+        expect(page).to have_content("Plant 2")
+        expect(page).to have_content("29.99")
+        expect(page).to have_xpath("//img[@src=\"/assets/plants/plant-4.jpg\"]")
+        expect(page).to have_content("Plant 3")
+        expect(page).to have_content("39.99")
+        expect(page).to have_xpath("//img[@src=\"/assets/plants/plant-3.jpg\"]")
       end
     end
   end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Category, type: :model do
   it "can have two products" do
@@ -20,16 +20,21 @@ RSpec.describe Category, type: :model do
       category_id: category.id)
 
     expect(category.name).to eq("Plants")
-    expect(category.description).to eq("The largest carnivorous plant selection in the world!")
+    expect(category.description).to eq(
+      "The largest carnivorous plant selection in the world!")
 
     expect(category.products.find(product_1.id).name).to eq("Venus Fly Trap")
-    expect(category.products.find(product_1.id).description).to eq("The gold standard of carnivorous plants!")
-    expect(category.products.find(product_1.id).image_url).to eq("venus_fly_trap.jpg")
+    expect(category.products.find(product_1.id).description).to eq(
+      "The gold standard of carnivorous plants!")
+    expect(category.products.find(product_1.id).image_url).to eq(
+      "venus_fly_trap.jpg")
     expect(category.products.find(product_1.id).price).to eq(19.99)
-    
+
     expect(category.products.find(product_2.id).name).to eq("Pitcher Plant")
-    expect(category.products.find(product_2.id).description).to eq("The silver standard of carnivorous plants!")
-    expect(category.products.find(product_2.id).image_url).to eq("pitcher_plant.jpg")
+    expect(category.products.find(product_2.id).description).to eq(
+      "The silver standard of carnivorous plants!")
+    expect(category.products.find(product_2.id).image_url).to eq(
+      "pitcher_plant.jpg")
     expect(category.products.find(product_2.id).price).to eq(14.99)
   end
 
