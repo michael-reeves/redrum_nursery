363f28b638885b45e046874514ec72534271c9a4
Samson Brock <SamsonLB@gmail.com>
2015-08-08 11:42:05 -0600

    Add User data scrubbing and validation checks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  before_validation :strip_whitespace
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
+  validates :password, length: { minimum: 8 }
+  validates_format_of :email,
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
+  private
+
+  def strip_whitespace
+    self.first_name = self.first_name.strip if self.first_name
+    self.last_name = self.last_name.strip if self.last_name
+    self.email = self.email.strip if self.email
+  end
 end
