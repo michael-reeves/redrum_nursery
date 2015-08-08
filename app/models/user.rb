ce58758f0c1b1bf72ec32b06257c5c13611063d6
Samson Brock <SamsonLB@gmail.com>
2015-08-08 15:04:39 -0600

    Remove redundant self calls from strip_whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ActiveRecord::Base
   private
 
   def strip_whitespace
-    self.first_name = self.first_name.strip if self.first_name
-    self.last_name = self.last_name.strip if self.last_name
-    self.email = self.email.strip if self.email
+    self.first_name = first_name.strip if first_name
+    self.last_name = last_name.strip if last_name
+    self.email = email.strip if email
   end
 end
