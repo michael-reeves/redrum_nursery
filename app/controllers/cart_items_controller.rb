8f851aa1265937d177b329322cc50f9d12a47fa5
Sebastian Abondano <sabondano1@gmail.com>
2015-08-08 11:28:18 -0600

    Removed two commented out byebugs

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,11 +1,9 @@
 class CartItemsController < ApplicationController
   def index
     @cart_items = cart.cart_items
-    # byebug
   end
 
   def create
-    # byebug
     product = Product.find(params[:product_id])
     flash[:success] = "#{product.name} added to cart"
     cart.add_item(product)
