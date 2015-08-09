2f3f276cd95a0e7216260d592f34954864190561
Michael Reeves <mreeves68@yahoo.com>
2015-08-09 03:03:56 -0600

    Refactor spec to limit hard coded values

--- spec/features/visitor_cart_spec.rb
+++ spec/features/visitor_cart_spec.rb
@@ -22,16 +22,16 @@ feature "Visitor" do
       expect(current_path).to eq(cart_path)
 
       within(".name") do
-        expect(page).to have_content("Plant 1")
+        expect(page).to have_content(item1.name)
       end
 
-      within(".row", text: "Plant 1") do
+      within(".row", text: item1.name) do
         quantity = find(".quantity").value
         expect(quantity).to eq("2")
       end
 
       within(".total") do
-        expect(page).to have_content("$39.98")
+        expect(page).to have_content("$#{item1.price * 2}")
       end
     end
 
@@ -53,18 +53,18 @@ feature "Visitor" do
       find("#cart").click
 
       expect(current_path).to eq(cart_path)
-      within(".row", text: "Plant 1") do
-        expect(page).to have_content("Plant 1")
+      within(".row", text: item1.name) do
+        expect(page).to have_content(item1.name)
         quantity = find(".quantity").value
         expect(quantity).to eq("2")
-        expect(page).to have_content("$39.98")
+        expect(page).to have_content("$#{item1.price}")
       end
 
-      within(".row", text: "Food 3") do
-        expect(page).to have_content("Food 3")
+      within(".row", text: item2.name) do
+        expect(page).to have_content(item2.name)
         quantity = find(".quantity").value
         expect(quantity).to eq("1")
-        expect(page).to have_content("$39.99")
+        expect(page).to have_content("$#{item2.price}")
       end
     end
 
@@ -78,58 +78,58 @@ feature "Visitor" do
 
       find("#cart").click
 
-      within(".row", text: "Plant 1") do
+      within(".row", text: item1.name) do
         find(".quantity").set("4")
         click_button("update")
       end
 
       expect(current_path).to eq(cart_path)
 
-      within(".row", text: "Plant 1") do
+      within(".row", text: item1.name) do
         quantity = find(".quantity").value
         expect(quantity).to eq("4")
-        expect(page).to have_content("$79.96")
+        expect(page).to have_content("$#{item1.price * 4}")
       end
 
       within(".total") do
-        expect(page).to have_content("$119.95")
+        expect(page).to have_content("$#{item1.price * 4 + item2.price}")
       end
     end
 
     scenario "adds an item twice and then decreases the quantity to one" do
-      item1 = @plants.products.first
-      visit product_path(item1)
+      item = @plants.products.first
+      visit product_path(item)
       click_button "Add to Cart"
       click_button "Add to Cart"
 
       find("#cart").click
-      within(".row", text: "Plant 1") do
+      within(".row", text: item.name) do
         quantity = find(".quantity").value
         expect(quantity).to eq("2")
-        expect(page).to have_content("$39.98")
+        expect(page).to have_content("$#{item.price * 2}")
       end
 
       within(".total") do
-        expect(page).to have_content("$39.98")
+        expect(page).to have_content("$#{item.price * 2}")
       end
 
-      within(".row", text: "Plant 1") do
+      within(".row", text: item.name) do
         find(".quantity").set("1")
         click_button("update")
       end
 
       expect(current_path).to eq(cart_path)
 
-      within(".row", text: "Plant 1") do
+      within(".row", text: item.name) do
         quantity = find(".quantity").value
         expect(quantity).to eq("1")
         within(".sub-total") do
-          expect(page).to have_content("$19.99")
+          expect(page).to have_content("$#{item.price}")
         end
       end
 
       within(".total") do
-        expect(page).to have_content("$19.99")
+        expect(page).to have_content("$#{item.price}")
       end
     end
 
@@ -139,33 +139,33 @@ feature "Visitor" do
       click_button "Add to Cart"
 
       find("#cart").click
-      within(".row", text: "Plant 1") do
+      within(".row", text: item.name) do
         quantity = find(".quantity").value
         expect(quantity).to eq("1")
-        expect(page).to have_content("$19.99")
+        expect(page).to have_content("$#{item.price}")
       end
 
       within(".total") do
-        expect(page).to have_content("$19.99")
+        expect(page).to have_content("$#{item.price}")
       end
 
-      within(".row", text: "Plant 1") do
+      within(".row", text: item.name) do
         click_link("remove")
       end
 
       expect(current_path).to eq(cart_path)
 
       within(".flash") do
-        expect(page).to have_content("Successfully removed Plant 1 " \
+        expect(page).to have_content("Successfully removed #{item.name} " \
           "from your cart")
-        expect(page).to have_link("Plant 1")
-        expect(page).to have_xpath("//a[@href=\"/products/77\"]")
+        expect(page).to have_link(item.name)
+        expect(page).to have_xpath("//a[@href=\"/products/#{item.id}\"]")
       end
 
-      expect(page).not_to have_content("This is the description for plant 1")
+      expect(page).not_to have_content(item.description)
 
       within(".total") do
-        expect(page).not_to have_content("$19.99")
+        expect(page).not_to have_content("$#{item.price}")
         expect(page).to have_content("$0")
       end
     end
