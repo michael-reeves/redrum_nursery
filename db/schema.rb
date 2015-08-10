1d5706d9aa3d1c98ca5382a6166ce419bf12e76c
Michael Reeves <mreeves68@yahoo.com>
2015-08-10 07:27:47 -0600

    Add status field to products table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150808215014) do
+ActiveRecord::Schema.define(version: 20150810132016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,11 +53,13 @@ ActiveRecord::Schema.define(version: 20150808215014) do
     t.text     "description"
     t.text     "image_url"
     t.decimal  "price",       precision: 8, scale: 2
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.integer  "status",                              default: 0
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+  add_index "products", ["status"], name: "index_products_on_status", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.text     "email"
