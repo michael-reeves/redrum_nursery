7a14040d4bbabaf45c92b496d9ce05ce335e23e7
Sebastian Abondano <sabondano1@gmail.com>
2015-08-12 17:46:30 -0600

    Add submit payment feature(stripe) upon checkout
    https://waffle.io/michael-reeves/redrum_nursery/cards/55c7f0773c538f320083ffca
    
    Why:
    Per user story #52, a user should submit a payment after clicking
    checkout while in the cart index view.
    
    This change addresses the need by:
    * adding ChargesController with a #create action that charges customer and triggers the
      creation of an order and order_items if the charge is successful
    * adding coniditional to cart index view that displays checkout button
      only for logged in users and users who have products in the cart
    * adding checkout custom stripe checkout button on cart index view
    * removing #create action in OrdersController as it's not needed anymore
    * removing create resource path in routes (not needed anymore)
    * modified existing tests for them to test new functionality
    
    Important notes: The user_creates_an_orders_spec test, that checks if a
    user can place an order, was skipped as the team is not clear if we
    should test the stripe integration. We're afraid of being shut down by
    Stripe if we call their API too often.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,30 +1,45 @@
 class ChargesController < ApplicationController
   def create
-    @order = Order.find(params[:order_id])
-    total = @order.total
+    # @order = Order.find(params[:order_id])
+    total = cart.total_price
     total_in_cents = total * 100
-    flash[:success] = "Your payment was submitted."
-    @order.update(status: "paid")
+    # @order.update(status: "paid")
 
     # Amount in cents
     @amount = total_in_cents.to_i
 
     customer = Stripe::Customer.create(
-      :email => @order.user.email,
+      :email => current_user.email,
       :card  => params[:stripeToken]
     )
 
     charge = Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => @amount,
-      :description => 'Rails Stripe customer',
+      :description => Order.last.id + 1,
       :currency    => 'usd'
     )
 
-    redirect_to order_path(@order)
+    if charge["paid"] == true
+      order = Order.create(user_id: current_user.id,
+                           status: "paid")
+
+      cart.cart_items.each do |cart_item|
+        OrderItem.create(order_id: order.id,
+                         product_id: cart_item.id,
+                         quantity: cart_item.quantity,
+                         unit_price: cart_item.price)
+      end 
+
+      session[:cart] = {}
+      cart.clear
+
+      flash[:success] = "Your payment was successful and your order is placed."
+      redirect_to order_path(order)
+    end
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
-    redirect_to orders_path
+    redirect_to cart_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, param: :slug, only: [:show]
   resources :users, only: [:index, :new, :create, :edit, :update]
-  resources :orders, only: [:index, :show, :create]
+  resources :orders, only: [:index, :show]
 
   get "/dashboard", to: "users#show"
 
